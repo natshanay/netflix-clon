a7b5b5a2c6c5e70106be6adbd2d9928d24a6588a
= <=>
2024-07-15 05:38:02 -0700

    jk

---
+++ natsh/natsh/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+// import Banner from "./Banner";
+import Footer from "./Footer";
+import Row from "../../src/Row";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Omishtu</h1>
+    </div>
+  );
+}
+
+export default App;
